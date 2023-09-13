a4270ed5ab7c66e0e764022604b35b52a7581ae0
Andriiok <137773359+Andriiok@users.noreply.github.com>
2023-09-13 21:43:27 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,126 @@
+from collections import UserDict
+
+
+class Field:
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return str(self.value)
+
+
+class Name(Field):
+    pass
+
+
+class Phone(Field):
+    def __init__(self, value):
+        super().__init__(value)
+        self.validate(self.value)
+
+    @staticmethod
+    def validate(value):
+        if not value.isdigit() or len(value) != 10:
+            raise ValueError("Invalid phone number format. Phone number must contain 10 digits.")
+
+    def __str__(self):
+        return self.value
+
+
+class Record:
+    def __init__(self, name):
+        self.name = Name(name)
+        self.phones = []
+
+    def add_phone(self, phone):
+        self.phones.append(Phone(phone))
+
+    def remove_phone(self, phone):
+        for p in self.phones:
+            if p.value == phone:
+                self.phones.remove(p)
+                break
+
+    def edit_phone(self, old_phone, new_phone):
+        phone_to_edit = self.find_phone(old_phone)
+        if phone_to_edit:
+            phone_to_edit.value = new_phone
+        else:
+            raise ValueError(f"Phone number {old_phone} not found in the record")
+
+    def find_phone(self, phone):
+        for p in self.phones:
+            if p.value == phone:
+                return p
+        return None
+
+    def __str__(self):
+        phones_str = '; '.join(str(p) for p in self.phones)
+        return f"Contact name: {self.name}, phones: {phones_str}"
+
+
+class AddressBook(UserDict):
+    def add_record(self, record):
+        self.data[record.name.value] = record
+
+    def delete(self, name):
+        if name in self.data:
+            del self.data[name]
+        elif isinstance(name, str):
+            for key in list(self.data.keys()):
+                if self.data[key].name.value == name:
+                    del self.data[key]
+                    return
+        else:
+            raise ValueError(f"Contact {name} not found")
+
+    def find(self, name):
+        for record in self.data.values():
+            if record.name.value == name:
+                return record
+        return None
+
+
+if __name__ == "__main__":
+    book = AddressBook()
+
+    john_record = Record("John")
+    john_record.add_phone("1234567890")
+    john_record.add_phone("5555555555")
+
+    book.add_record(john_record)
+
+    jane_record = Record("Jane")
+    jane_record.add_phone("9876543210")
+    book.add_record(jane_record)
+
+    for name, record in book.data.items():
+        print(record)
+
+    john = book.find("John")
+    john.edit_phone("1234567890", "1112223333")
+
+    print(john)
+
+    found_phone = john.find_phone("5555555555")
+    print(f"{john.name.value}: {found_phone}")
+
+    book.delete("Jane")
+
+    for name, record in book.data.items():
+        print(record)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
